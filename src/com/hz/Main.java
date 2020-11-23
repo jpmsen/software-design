8a1287f6f89ae0d29d7f7cbc95feb8a62f9515f4
jpmsen <52090304+jpmsen@users.noreply.github.com>
2020-11-23 10:51:21 +0100

    add digital clock

--- src/com/hz/Main.java
+++ src/com/hz/Main.java
@@ -1,44 +1,21 @@
 package com.hz;
 
-import java.awt.geom.NoninvertibleTransformException;
-import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
+	    // write your code here
         ConsoleReader reader = new ConsoleReader();
         ConsoleWriter writer = new ConsoleWriter();
 
-        // ask questions
-        String q1 = "Do you like eating vegetables?";
 
-        String good = "You healthy person!";
-        String bad = "Maybe you should try to eat some carrots";
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter formattedTime = DateTimeFormatter.ofPattern("dd-MM-yyy HH:mm:ss.SS");
 
-        // ask question to user
-        writer.write(q1);
-
-        // create list with all accepted answers
-        ArrayList<String> list = new ArrayList<String>();
-        Collections.addAll(list, "true", "yes", "oh yeah", "great", "sure", "love to", "of course", "always", "never done otherwise");
-
-        // create answer class and use an adapter to translate answers into a boolean.
-        Answer answer = new Answer(list);
-        AnswerAdapter answerAdapter = new AnswerAdapter(answer);
-
-        // read response
-        Boolean ans1 = answerAdapter.checkAnswer(reader.readLine());
-
-        if (ans1) {
-            writer.write(good);
-        } else {
-            writer.write(bad);
-        }
+        writer.write(time.format(formattedTime));
     }
 }
